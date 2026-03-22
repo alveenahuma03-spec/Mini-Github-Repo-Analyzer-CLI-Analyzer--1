ab4b335c7f93eefa3a1f30bdbc1d88b98fb7709d
alveenahuma03-spec <alveenahuma03@gmail.com>
2026-03-23 03:08:25 +0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,21 +2,24 @@ import requests
 
 repository_url = input("Enter GitHub repository URL: ")
 
-# extract owner/repo
+# splits the URL to get owner and repository name
 part = repository_url.strip().split("/")
 owner = part[-2]
 repository = part[-1]
 
-# GitHub API
+# calls GitHub API to get languages
 url = f"https://api.github.com/repos/{owner}/{repository}/languages"
 res = requests.get(url)
 
+#Checks if API request worked, it it did not, it stops the program
 if res.status_code != 200:
     print("Invalid repo")
     exit()
 
+# converts the API response into a Python dictionary
 languages = res.json()
 
+#if any languages are founded, it prints them
 if languages:
     print("\nRepo:", repository)
     print("Languages found:")
